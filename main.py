4eee95d71755543199f1c87be0c9f1297e038a66
Th3R4nd0m <strugaruemanuel@gmail.com>
2020-07-07 19:50:02 +0000

    Added ports list

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 import sys
+import re
 
 ip = sys.argv[1]
-optional_name = sys.argv[2]
 
-if len(optional_name) > 0:
-    os.system("mkdir " + optional_name + " & cd " + optional_name )
-else: 
-    os.system("mkdir " + ip + " & cd " + ip )
+#optional_name = sys.argv[2]
 
-os.system("mkdir nmap & nmap -p- " + ip + " -oN nmap/nmapAllPorts")
+#if len(optional_name) > 0:
+#   os.system("mkdir " + optional_name + " & cd " + optional_name )
+#else:
+os.system("mkdir " + ip + " ; cd " + ip )
+
+os.system("mkdir nmap ; nmap -p- " + ip + " -oN nmap/nmapAllPorts")
 os.system("nmap -A -sV " + ip + " -oN nmap/nmapInDetail")
 
-os.system("cd ../ & nikto -h http://" + ip + ":" + port + " > nikto")
+ports_match = re.compile("(\d+)\/tcp")
+ports =  re.findall(ports_match, open("nmap/nmapAllPorts", b"r").read())
+print(ports)
+os.system("nikto -h http://" + ip + ":" + ports[0] + " > nikto")
 
