8e19a180b505734ed839ea50f4cd7f8134d33ae1
joswha <vladtoie@gmail.com>
2020-07-07 23:46:58 +0300

    nmap and nikto work; ports list added

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import sys
 import re
 
 ip = sys.argv[1]
-
 #optional_name = sys.argv[2]
 
 #if len(optional_name) > 0:
 #   os.system("mkdir " + optional_name + " & cd " + optional_name )
 #else:
-os.system("mkdir " + ip + " ; cd " + ip )
+os.mkdir(ip)
 
+os.chdir(ip)
 os.system("mkdir nmap ; nmap -p- " + ip + " -oN nmap/nmapAllPorts")
 os.system("nmap -A -sV " + ip + " -oN nmap/nmapInDetail")
 
 ports_match = re.compile("(\d+)\/tcp")
-ports =  re.findall(ports_match, open("nmap/nmapAllPorts", b"r").read())
+ports =  re.findall(ports_match, open("nmap/nmapAllPorts", "r").read())
 print(ports)
 os.system("nikto -h http://" + ip + ":" + ports[0] + " > nikto")
-
