cb986bac51fa90985272905d19fc6323aeeb4d7a
joswha <vladtoie@gmail.com>
2020-07-08 23:33:01 +0300

    added flag -t for ip address

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import os
+from os import path
 import sys
 import re
+import subprocess
+import argparse
 
-ip = sys.argv[1]
-#optional_name = sys.argv[2]
+########################################################################################################
+#                                       hey                                                            #
+########################################################################################################
 
-#if len(optional_name) > 0:
-#   os.system("mkdir " + optional_name + " & cd " + optional_name )
-#else:
-os.mkdir(ip)
+
+parser = argparse.ArgumentParser(description='Recon automation boi')
+
+parser.add_argument('--t', type=str, help='Target(ip address)', required=True, action="store", dest="ip")
+
+args = parser.parse_args()
+ip = args.ip
+
+if not path.exists(ip):
+    os.mkdir(ip)
+else:
+    print("There already exists a directory for that target... continuing")
 
 os.chdir(ip)
-os.system("mkdir nmap ; nmap -p- " + ip + " -oN nmap/nmapAllPorts")
-os.system("nmap -A -sV " + ip + " -oN nmap/nmapInDetail")
 
-ports_match = re.compile("(\d+)\/tcp")
-ports =  re.findall(ports_match, open("nmap/nmapAllPorts", "r").read())
-print(ports)
-os.system("nikto -h http://" + ip + ":" + ports[0] + " > nikto")
+if not path.exists('nmap'):
+    os.mkdir('nmap')
+else:
+    print("There already exists a nmap directory for that target... continuing")
+
+first = subprocess.Popen("nmap -p- " + ip + " -oN nmap/nmapAllPorts", shell = True)
+
+while True:
+    if first.poll() is not None:
+        ports_match = re.compile("(\d+)\/tcp")
+        ports =  re.findall(ports_match, open("nmap/nmapAllPorts", "r").read())
+        all_ports = ",".join(ports)
+
+        second = subprocess.Popen("nmap -p " + all_ports + " " + ip + " -A -sV -oN nmap/nmapInDetail", shell = True)
+        
+        print(all_ports)
+        break
+
+# os.system("nikto -h http://" + ip + ":" + ports[0] + " > nikto")
