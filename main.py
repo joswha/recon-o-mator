edcf9be7a5b55488b065bf334d981cd8c25fa1fb
joswha <vladtoie@gmail.com>
2020-07-09 21:35:23 +0300

    added nikto and dirsearch capabilities

--- main.py
+++ main.py
@@ -21,30 +21,30 @@ def make_directory(loc):
     if not path.exists(loc):
         os.mkdir(loc)
     else:
-        print("There already exists a directory for that target... continuing")
+        print("There already exists a " + loc + " directory for that target... continuing")
 
 make_directory(ip)
 os.chdir(ip)
 
 make_directory('nmap')
+make_directory('nikto')
+make_directory('dirsearch')
 
-first = subprocess.Popen("nmap " + ip + " -oN nmap/nmapAllPorts", shell = True)
+# first = subprocess.Popen("nmap " + ip + " -oN nmap/nmapAllPorts", shell = True)
 
 def find_port(port_type):
-    ports =  re.findall(re.compile("(\d+)\/tcp" + port_type), open("nmap/nmapAllPorts", "r").read())
-    return ",".join(ports)
+    ports =  re.findall(re.compile("(\d+)\/tcp" + port_type), open("nmap/nmapHTTPports", "r").read())
+    return ports
 
 while True:
     if first.poll() is not None:
-        all_ports = find_port('')
-        http_ports = find_port(".+http")
-        domain_ports = find_port(".+domain")
-        
-        # second = subprocess.Popen("nmap -p " + all_ports + " " + ip + " -A -sV -oN nmap/nmapInDetail", shell = True)
-        
-        print(all_ports)
-        print(http_ports)
-        print(domain_ports)
-        break
+        all_ports = ",".join(find_port('')) # ports for detailed NMAP, separated by comma
+        http_ports = find_port(".+http")    # ports for HTTP related commands, not separated by comma
+    
+        second = subprocess.Popen("nmap -p " + all_ports + " " + ip + " -A -sV -oN nmap/nmapInDetail", shell = True)
 
-# os.system("nikto -h http://" + ip + ":" + ports[0] + " > nikto")
+        for port in http_ports:
+            nikto = subprocess.Popen("nikto -h http://" + ip + ":" + port + " > nikto_" + port, shell = True)
+            dirsearch = subprocess.Popen("python dirsearch.py -u http://" + ip + ":" + port + " -e txt,php,js > dirsearch_" + port, shell = True)
+
+        break
